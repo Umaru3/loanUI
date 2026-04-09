a4e53c23afd7e138a91fa627b03d4547340cfd95
VEV\justi <justincastro105@gmail.com>
2026-04-09 13:56:14 +0800

    userBranch-001: [FEATURE][ALL] - update deprecated safeview area

--- src/components/SafeAreaWrapper.js
+++ src/components/SafeAreaWrapper.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import { StatusBar, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function SafeAreaWrapper({ children }) {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
-      {children}
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
+        {children}
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: "#f2f2f2", // your global background color
+    backgroundColor: "#f2f2f2",
   },
 });
